6f7834a5f1d630c73b6dbed97074d71431237032
andywilsonuk <git@andywilson.me.uk>
2021-03-13 10:55:52 +0000

    Corrected in-app description of project

--- src/components/description.jsx
+++ src/components/description.jsx
@@ -2,6 +2,6 @@ import h from 'hyperapp-jsx-pragma';
 
 export default () => (
     <div>
-        <h1>hyperapp2-webpack4-boilerplate</h1>
+        <h1>hyperapp2-webpack-boilerplate</h1>
     </div>
 );
